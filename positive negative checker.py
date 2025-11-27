6624b678756ce27dbdfc0e9d77cc0b34d1d7ec28
Muskan Khan <muskankhan1073615@gmail.com>
2025-11-27 09:40:00 +0530

    Add number classification program using conditional statements

---
+++ positive negative checker.py
@@ -0,0 +1,8 @@
+num = float(input("enter a number: "))
+
+if num > 0:
+    print("the number is positive")
+elif num < 0:
+    print("the humber is negative")
+else:3
+    print("the humber is zero")
